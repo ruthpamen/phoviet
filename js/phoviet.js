e5f07a26a7e54a45eed032d6a86f7ffac534192b
ruthpamen <44677565+ruthpamen@users.noreply.github.com>
2020-12-21 22:59:04 +0100

    second commit

--- js/phoviet.js
+++ js/phoviet.js
@@ -84,3 +84,13 @@
 	});
 
 })
+
+$('.popup-link').magnificPopup({
+	type: 'image',
+	mainClass: 'mfp-with-zoom', 
+	zoom: {
+	  enabled: true, 
+	  duration: 300, 
+	  easing: 'ease-in-out', 
+	}
+  });
