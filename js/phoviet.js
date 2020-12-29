ac4dcb337588c2287b3fb046150da62f343038cd
ruthpamen <44677565+ruthpamen@users.noreply.github.com>
2020-12-29 14:31:32 +0100

    add gallery section

--- js/phoviet.js
+++ js/phoviet.js
@@ -1,119 +1,4 @@
 
-(function ($) {
-    "use strict";
-    
-	/*==============================================[ scroll one page ]==*/
-	$(document).ready(function() {
-
-		$('#next').click(function (){
-  
-			if($('.on-screen').next().length == 0){
-			  $('.on-screen').removeClass('on-screen come-in').addClass('out-screen');
-			  $('.image-container').first('image-container').removeClass('out-screen').addClass('come-in on-screen');
-			}
-			else
-			  $('.on-screen').removeClass('on-screen come-in').addClass('out-screen').next('.image-container').addClass('come-in on-screen');
-		  });
-		  $('#prev').click(function (){
-			
-			if($('.on-screen').prev().length == 0){
-			  $('.on-screen').removeClass('on-screen come-in').addClass('out-screen');
-			  $('.image-container').last('image-container').removeClass('out-screen').addClass('come-in on-screen');
-			}
-			else
-			  $('.on-screen').removeClass('on-screen come-in').addClass('out-screen').prev('.image-container').addClass('come-in on-screen');
-		  });
-		$('.home-onepage-menu-bar a, .home-onepage-menu-bar-top a, .home-onepage-menu-overlay a').on('click', function(event){
-			event.preventDefault();
-			$('html, body').animate({scrollTop: $($(this).attr('href')).offset().top}, 500);
-		});
-		
-		var posMenuBar = $('.home-onepage-menu-bar').offset().top;
-
-		var posIntroTop = $('#home-onepage-intro').offset().top;
-		var posIntroBottom = $('#home-onepage-intro').offset().top + $('#home-onepage-intro').height() + 100;
-
-		var posMenuTop = $('#home-onepage-our-menu').offset().top;
-		var posMenuBottom = $('#home-onepage-our-menu').offset().top + $('#home-onepage-our-menu').height() + 100;
-
-		var posEventTop = $('#home-onepage-event').offset().top;
-		var posEventBottom = $('#home-onepage-event').offset().top + $('#home-onepage-event').height() + 100;
-
-		var posReservationTop = $('#home-onepage-reservation').offset().top;
-		var posReservationBottom = $('#home-onepage-reservation').offset().top + $('#home-onepage-reservation').height() + 100;
-
-		var posContactTop = $('#home-onepage-contact').offset().top;
-		var posContactBottom = $('#home-onepage-contact').offset().top + $('#home-onepage-contact').height();
-
-		var posPageBottom = $('#home-onepage-contact').offset().top - ($(window).height() - $('#home-onepage-contact').height());
-
-		$(window).on('scroll',function(){
-			showActiveMenu();
-			if($(this).scrollTop() > posMenuBar && $(this).width() > 992) { 
-				$('.home-onepage-wrap-menu-bar').css('display', 'none');
-				$('.home-onepage-wrap-menu-bar-top').css('display', 'block');
-			}
-			else {
-				$('.home-onepage-wrap-menu-bar').css('display', 'block');
-				$('.home-onepage-wrap-menu-bar-top').css('display', 'none');
-			}
-		});
-
-
-		function  showActiveMenu() {
-
-			if($(window).scrollTop() > posIntroTop-67 && $(window).scrollTop() < posIntroBottom) {
-				$('.home-onepage-intro').addClass('active-menu-bar');
-			}
-			else {
-				$('.home-onepage-intro').removeClass('active-menu-bar');
-			}
-
-			if($(window).scrollTop() > posMenuTop-67 && $(window).scrollTop() < posMenuBottom) {
-				$('.home-onepage-our-menu').addClass('active-menu-bar');
-			}
-			else {
-				$('.home-onepage-our-menu').removeClass('active-menu-bar');
-			}
-
-			if($(window).scrollTop() > posEventTop-67 && $(window).scrollTop() < posEventBottom) {
-				$('.home-onepage-event-2').addClass('active-menu-bar');
-			}
-			else {
-				$('.home-onepage-event-2').removeClass('active-menu-bar');
-			}
-
-			if($(window).scrollTop() > posReservationTop-67 && $(window).scrollTop() < posReservationBottom) {
-				$('.home-onepage-reservation').addClass('active-menu-bar');
-			}
-			else {
-				$('.home-onepage-reservation').removeClass('active-menu-bar');
-			}
-
-			if(($(window).scrollTop() > posContactTop-67 && $(window).scrollTop() < posContactBottom) || $(window).scrollTop() >= posPageBottom) {
-				$('.home-onepage-contact').addClass('active-menu-bar');
-				$('.home-onepage-reservation').removeClass('active-menu-bar');
-			}
-			else {
-				$('.home-onepage-contact').removeClass('active-menu-bar');
-			}
-
-		}
-	});
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
 $('#next').click(function (){
   
 	if($('.on-screen').next().length == 0){
@@ -132,3 +17,24 @@ $('#next').click(function (){
 	else
 	  $('.on-screen').removeClass('on-screen come-in').addClass('out-screen').prev('.image-box').addClass('come-in on-screen');
   });
+
+
+
+
+
+  $(document).ready(function(){
+	$(".fancybox").fancybox({
+		  openEffect: "none",
+		  closeEffect: "none"
+	  });
+	  
+	  $(".zoom").hover(function(){
+		  
+		  $(this).addClass('transition');
+	  }, function(){
+		  
+		  $(this).removeClass('transition');
+	  });
+  });
+	  
+
