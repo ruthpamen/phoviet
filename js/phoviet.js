8d5fda8f7ab6b8a2be770fac852f03dd9171e4a2
ruthpamen <44677565+ruthpamen@users.noreply.github.com>
2021-01-20 18:31:30 +0100

    first final version

--- js/phoviet.js
+++ js/phoviet.js
@@ -85,25 +85,6 @@
 })
 
 
-$('#next').click(function (){
-  
-	if($('.on-screen').next().length == 0){
-	  $('.on-screen').removeClass('on-screen come-in').addClass('out-screen');
-	  $('.image-box').first('image-box').removeClass('out-screen').addClass('come-in on-screen');
-	}
-	else
-	  $('.on-screen').removeClass('on-screen come-in').addClass('out-screen').next('.image-box').addClass('come-in on-screen');
-  });
-  $('#prev').click(function (){
-	
-	if($('.on-screen').prev().length == 0){
-	  $('.on-screen').removeClass('on-screen come-in').addClass('out-screen');
-	  $('.image-box').last('image-box').removeClass('out-screen').addClass('come-in on-screen');
-	}
-	else
-	  $('.on-screen').removeClass('on-screen come-in').addClass('out-screen').prev('.image-box').addClass('come-in on-screen');
-  });
-
 
 
 
